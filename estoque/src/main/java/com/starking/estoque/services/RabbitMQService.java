c5f2ff86085c463d5198d8ff8c8aeb158cfeda33
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-04-13 19:21:05 -0300

    Convertando a msg que estava em bits com mappers

--- estoque/src/main/java/com/starking/estoque/services/RabbitMQService.java
+++ estoque/src/main/java/com/starking/estoque/services/RabbitMQService.java
@@ -4,14 +4,24 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class RabbitMQService {
-	
+
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
-	
-	public void enviaMensagem(String nomeFila, Object mensagem) {
-		this.rabbitTemplate.convertAndSend(nomeFila, mensagem);
-	}
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	public void enviaMensagem(String nomeFila, Object mensagem) {
+		try {
+			String writeValueAsString = this.objectMapper.writeValueAsString(mensagem);
+			this.rabbitTemplate.convertAndSend(nomeFila, writeValueAsString);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+	}
 }
